3bff4fa8bdf9c7efda6966f03af899e05a6d545e
SamHein8 <97979982+SamHein8@users.noreply.github.com>
2022-02-25 11:34:22 -0500

    Add files via upload

--- mesFonctions.js
+++ mesFonctions.js
@@ -16,6 +16,147 @@ window.onload = function load(){
     }
 }
 
+class Book {
+  constructor(title,author){
+      this.title = title;
+      this.author = author;
+  }
+}
+
+
+class UI {
+
+
+  static displayBooks(){
+      let books = Store.getBooks();
+
+      books.forEach((book) => UI.addBookToList(book));
+
+     
+  }
+
+  static addBookToList(book){
+      const list = document.querySelector('#book-list');
+
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+
+      list.appendChild(row);
+  }
+
+
+  static clearFields(){
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+
+ 
+  static deleteBook(el){
+      if(el.classList.contains('delete')){
+          el.parentElement.parentElement.remove();
+      }
+  }
+
+  
+  static showAlerts(message,className){
+    
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+     
+      div.appendChild(document.createTextNode(message));
+      
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      container.insertBefore(div,form);
+
+     
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+}
+
+
+class Store {
+  static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+
+  static saveBook(book){
+      let books;
+      books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books',JSON.stringify(books));
+  }
+
+  static removeBook(isbn){
+      const books = Store.getBooks();
+
+      books.forEach((book, index) => {
+        if(book.isbn === isbn) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded',UI.displayBooks);
+
+
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+  
+
+  e.preventDefault();
+
+ 
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  if(title === '' || author === ''){
+      UI.showAlerts('Please fill in all details...','danger');
+  } else {
+      
+      const book = new Book(title,author);
+
+    
+      UI.addBookToList(book);
+
+     
+      Store.saveBook(book);
+
+ 
+      UI.showAlerts('Book Added','success');
+
+ 
+      UI.clearFields();
+  }
+
+});
+
+
+document.querySelector('#book-list').addEventListener('click',(e) => {
+  UI.deleteBook(e.target);
+
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  UI.showAlerts('Book Removed','success');
+});
+
+
+
 function verifierCaractere(){
     var regex = /^[a-zA-Z\éêëèïîôöûùÉçÇâàáa\n\s\,.]+$/i;
     var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
@@ -32,32 +173,31 @@ function dec2hex (dec) {
   
   // generateId :: Integer -> String
   function generateId (len) {
-    var arr = new Uint8Array((len || 400) / 2);
+    var arr = new Uint8Array((len || 200) / 2);
     window.crypto.getRandomValues(arr);
     return Array.from(arr, dec2hex).join('');
   }
 function genererCle(){
-    document.getElementById("SecretKey").value = generateId();
+    document.getElementById("title").value = generateId();
 }
 
-function ajouterAuCarnet(){
-    var CleId = document.getElementById("SecretKey");
-    if(CleId.value != ''){
-       /* var object = new jsonCarnetContact(CleId.value);
-        carnetContact.push(object);
-        localStorage["carnet"] = JSON.stringify(carnetContact);*/
-        if(localStorage.getItem("carnet") === undefined){
-            window.localStorage.setItem("carnet",JSON.stringify(CleId.value)); 
-        }
-        else{
-            window.localStorage.setItem("carnet",old + ";" + CleId.value); 
-        }
-        afficherCarnet();
-    }
-
+/*function ajouterAuCarnet(){
+    var form  = document.getElementById('contact');
+    if(!window.contactList){ //check if we already have a contact list
+        window.contactList=$ab(form.person.value,form.email.value);
+       } else {
+       //saves new values rather than deleting old ones as well
+         contactList.addNewContact(form.person.value,form.email.value);
+       }
+       
+         form.person.value = '';
+         form.email.value = '';
+       
+        event.preventDefault();
 }
+*/
 
-function afficherCarnet(){
+/*function afficherCarnet(){
     /*var monCarnet = document.getElementById("mesContacts").value;
     if(localStorage["carnet"] === undefined){
         localStorage["carnet"] = '';
@@ -72,11 +212,12 @@ function afficherCarnet(){
             monCarnet.innerHTML += str;
        }
 
-    }*/
+    }
     var messagecarnet =  window.localStorage.getItem("carnet");
     document.getElementById("mesContacts").innerHTML = messagecarnet;
 
-}
+}*/
+
 function envoyerMessage(){
     var newMessage = document.getElementById("message").value;
     var lastMessage = localStorage.getItem("datasMessage");
@@ -117,6 +258,17 @@ function boutonNouveau(){
     document.getElementById("principal").style.display = "none";
     document.getElementById("mesContacts").style.display = "none";
     document.getElementById("listedesMessages").style.display = "none";
+    if(!window.contactList){ //check if we already have a contact list
+        window.contactList=$ab(form.person.value,form.email.value);
+       } else {
+       //saves new values rather than deleting old ones as well
+         contactList.addNewContact(form.person.value,form.email.value);
+       }
+       
+         form.person.value = '';
+         form.email.value = '';
+       
+        event.preventDefault();
 }
 
 function boutonBoite(){
@@ -137,45 +289,17 @@ function boutonCarnet(){
     document.getElementById("principal").style.display = "none";
     document.getElementById("mesContacts").style.display = "block";
     document.getElementById("listedesMessages").style.display = "none";
-    afficherCarnet();
+    if(window.contactList){ //check if we already have a contact list
+        document.getElementById('mesContacts').innerHTML = '';
+      var contacts = contactList.returnAll();
+       console.log(contacts);
+       if(contacts.length>0){
+         for(var i = 0;i<contacts.length;i++){
+         document.getElementById('mesContacts').innerHTML += '<div class="contact-item">Name:'+contacts[i].name +'<br>Email:'+contacts[i].email+'</div><hr>';
+         }
+       }else{
+         document.getElementById('mesContacts').innerHTML += '<div class="contact-item">You have no contacts. Why not add  a few?</div><hr>';
+       }
+     }
 
 }
-
-
-
-/*
-class AddressBook{
-    constructor(){
-        const initData = [
-            {Name: 'Test', Key: SubtleCrypto.generateKey()},
-        ];
-        this.contactList = localStorage.contactList ? JSON.parse(localStorage.contactList) : initData;
-        this.Enregistrer();
-    }
-    Enregistrer(){
-        //Local storage
-        localStorage.contactList = JSON.stringify(this.contactList);
-    }
-    Nouveau(name){
-        //New contact
-        key = SubtleCrypto.generateKey();
-        const contact = { name,key };
-        this.contactList.push(contact);
-        this.Enregistrer();
-    }
-    Supprimer(index){
-        //Delete contact
-        this.contactList.splice(index,1);
-        this.Enregistrer();
-    }
-    search(condition){
-        //Search according to conditions, no matter whether you entered the name or phone number, you can match
-        const reg = new RegExp(condition);
-        return this.contactList.filter( item => reg.test(item.name) || reg.test(item.key));
-    }
-    getAllData(){
-        //Return to all data
-        document.write(this.contactList);
-        return this.contactList;
-    }
-}*/
